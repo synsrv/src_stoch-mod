8a880f09655f463f9b050767007cb48ef3c22f28
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2019-09-18 15:08:13 +0200

    add new figures

--- run_analysis.sh
+++ run_analysis.sh
@@ -6,6 +6,9 @@ cp -r ~/lab/stoch-mod/analysis-dev/ code/analysis/
 #python -m code.post_process.equal_dt
 #python -m code.post_process.fixed_start_dt
 
+python -m code.analysis.figures.xt_trace
+python -m code.analysis.figures.pdf_estimate_log
+
 python -m code.analysis.figures.synsrv_trace
 python -m code.analysis.figures.k_effect_equal
 python -m code.analysis.figures.npool_effect_equal

d50907277f958451555392b91b22c2ff39b4be6d
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2020-03-26 13:37:53 +0100

    add single traces figure

--- run_analysis.sh
+++ run_analysis.sh
@@ -6,9 +6,10 @@ cp -r ~/lab/stoch-mod/analysis-dev/ src/analysis/
 #python -m src.post_process.equal_dt
 #python -m src.post_process.fixed_start_dt
 
-python -m src.analysis.figures.xt_trace
-python -m src.analysis.figures.pdf_estimate_log
-
 python -m src.analysis.figures.synsrv_trace
 python -m src.analysis.figures.k_effect_equal
 python -m src.analysis.figures.npool_effect_equal
+
+python -m src.analysis.figures.xt_trace
+python -m src.analysis.figures.single_traces
+python -m src.analysis.figures.pdf_estimate_log
